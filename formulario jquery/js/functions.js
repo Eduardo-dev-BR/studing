7ed7bb170369c157dd1daf5f7b2b2c44e2198670
Eduardo <webdu15@gmail.com>
2020-09-09 06:31:56 -0300

    progresso 1

--- formulario jquery/js/functions.js
+++ formulario jquery/js/functions.js
@@ -1,3 +1,13 @@
 $(function(){
-    alert('hello world');
+    //inserindo texto em uma tag com formulario
+    $('.form_contato').submit(function(){   
+        //var input = $('.form_contato input[type=text]');
+        var container = $('.container');
+        
+        container.html(content);
+       // container.html(input.val());//o val faz retornar pelo valor do elemento selecionado
+
+    return false;
+    })
+
 });

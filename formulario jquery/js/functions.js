48e6d2045b693600f4138d48988ab29054ab4ddc
Eduardo <webdu15@gmail.com>
2020-09-14 21:39:05 -0300

    estudos
    atualizações - novos aprendizados

--- formulario jquery/js/functions.js
+++ formulario jquery/js/functions.js
@@ -1,12 +1,13 @@
 $(function(){
     //inserindo texto em uma tag com formulario
     $('.form_contato').submit(function(){   
-        //var input = $('.form_contato input[type=text]');
+        // var input = $('.form_contato input[type=text]');
         var container = $('.container');
-        
-        container.html(content);
-       // container.html(input.val());//o val faz retornar pelo valor do elemento selecionado
+        var content = 'Nome: ' + $('input[name=nome]').val() +
+        '<hr>E-mail: '+ $('input[name=email]').val() +
+        '<hr>Telefone: ' + $('input[name=telefone]').val();
 
+       container.html(content);//o val faz retornar pelo valor do elemento selecionado
     return false;
     })
 

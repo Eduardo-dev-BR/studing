6281a91545f47ad5da0444738368fc9c408a37c9
eduzzobe <70654895+eduzzobe@users.noreply.github.com>
2020-12-01 17:00:10 -0300

    Modulo D Start
    Solve the exercise 010

---
+++ curso em video/modulo D condicoes/aula11/ex008.js
@@ -0,0 +1,11 @@
+console.log('Olá mundo!')
+
+var vel = 70
+
+console.log(`A velocidade do seu carro é ${vel}hm/h`)
+
+if (vel >= 60){ //condição simples
+    console.log("_Voce ultrapassou a velocidade permitida. MULTADO!")
+}
+console.log("Dirija sempre usando cinto de segurança")
+

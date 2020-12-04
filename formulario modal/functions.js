8ae17275ec5399108f25b20524f956e0faf6bf96
Eduardo <webdu15@gmail.com>
2020-12-04 00:51:47 -0300

    Validação de formulário em Jquery
    Travei foda nessa merda :(

--- formulario modal/functions.js
+++ formulario modal/functions.js
@@ -5,59 +5,84 @@ $(function(){
 
     abrirJanela();
     verificarCliqueFechar();
-  
+
+    // var frase = "Olá mundo!"
+    // var ver = frase.match(/Ola/)
+    
+    // if(typeof(ver) == 'object' && ver != null){
+    //   console.log("Encontramos algo!")
+    // } else{
+    //   console.log("!Não encontramos nada!")
+    // }
+
+
+  //exemplo de validação de email
+    // var frase = 'eduardo@hotmail.com';
+    // var verifica = frase.match(/^(.*?)@(.*?)$/); // ^ -> garante que vai pegar do inicio // $ <- garante que vai pegar até o final
+
+    // if (verifica != null){
+    //   console.log("Encontramos algo!");
+    //   console.log(verifica[1])
+    //   console.log(verifica[2])
+    //   console.log(verifica[3])
+    // }else{
+    //   console.log("Não encontramos nada")
+    // }
+    
     function abrirJanela(){
         $('.btn').click(function(e){
           e.stopPropagation();
             $('.bg').fadeIn();
         });
     }
-    
     function verificarCliqueFechar(){
-      var el = $('body, .closeBtn')
+
+      var el = $('body,.closeBtn')
+
       el.click(function(){
         $('.bg').fadeOut();
       })
     }
 
-    var but = $('.closeBtn')
-    but.hover(function(){
-      $('.closeBtn').css('background','darkgray')
+    $('.form').click(function(e) {
+      e.stopPropagation();
     })
 
-      /*---------------|Expressões regulares|-----------------*/
-    // var frase = 'Olá mundo';
-    // var verifica = frase.match(/Olá/); //parametros case sensitive - descrever exatamente como é
-
-    // // console.log(typeof(verifica));
-    
-    // if(verifica != null){
-    //   console.log("Encontramos algo!");
-    // }else{
-    //   console.log("Não encontramos nada")
-    // }
-    /*-------------*/
-    // var frase = 'eduardo@hotmail.com';
-    // var verifica = frase.match(/[a-z]{3}/); //se tiver pelo menos 3 caracteres
-    // var verifica = frase.match(/[a-z]{3,6}/);
-    //var verifica = frase.match(/(.*?)@(.*?)/); // (.*?) -> pegue tudo que tem até encontrar o que está no meio
-    // var verifica = frase.match(/^^(.*?)@(.*?)$/); // ^^(.*?) -> começando a procurar do inicio. -  após achar continue daqui(.*?) $ -> garante que vai até o final
-
-
-    // if(verifica != null){
-      // console.log("Encontramos algo!");
-      // console.log(verifica[1]); //o que tem antes do @
-      // console.log(verifica[2]); //o que tem depois do @
-      // console.log(verifica[3]); //o que tem no final
-
-    // }else{
-      // console.log("Não encontramos nada")
-    // }
+    // var but = $('.closeBtn')
+    // but.hover(function(){
+    //   $('.closeBtn').css('background','darkgray')
+    // })
 
 
 // /*---------------|Validando formulário na prática|-----------------------*/
+$('form#form1').submit(function(e){
+  e.preventDefault();
+    var nome = $('input[name=nome]').val()
+    var telefone = $('input[name=telefone]').val()
+    var email = $('input[name=email]').val()
 
-         
+    var amount = nome.split(' ').lenght // contando a quantidade de espaços
+    var splitStr = nome.split(' ')
+    if(amount >= 2){
+      console.log('Deu certo!')
+      for(var i = 0; i < amount; i++ ){
+        if(splitStr[i].match(/^[A-Z]{1}[a-z]{1,}+$/)){
+          
+        }else{
+          $('input[name=nome]').css('border','2px solid red')
+          $('input[name=nome').data('invalido','true')     
+          return false;
+        }
+      }
+    }else{
+      $('input[name=nome]').css('border','2px solid red')
+      $('input[name=nome').data('invalido','true')     
+      return false;
+    }
+
+    return  false;
+
+})
 
 
 });

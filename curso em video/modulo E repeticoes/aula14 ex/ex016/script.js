67833f2cda7ee8afd230ebbd5198a33b40ada430
eduzzobe <70654895+eduzzobe@users.noreply.github.com>
2020-12-14 13:21:59 -0300

    contador inicio

---
+++ curso em video/modulo E repeticoes/aula14 ex/ex016/script.js
@@ -0,0 +1,11 @@
+function contagem(){
+    var num = 0
+    var numEsc = document.querySelector('#numEsc')
+    
+    for(c=0;num <= 20;c++){
+        var res = document.querySelector('#res')
+        var cnum = document.createElement('p',num)
+        res.innerHTML = cnum
+    
+    }
+}

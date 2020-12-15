91a0bccc15cee5c520d4fcbc6f1646e2c5454cc4
Eduardo <webdu15@gmail.com>
2020-12-15 07:10:32 -0300

    atz 2

--- curso em video/modulo E repeticoes/aula14 ex/ex016/script.js
+++ curso em video/modulo E repeticoes/aula14 ex/ex016/script.js
@@ -1,11 +1,8 @@
-function contagem(){
-    var num = 0
-    var numEsc = document.querySelector('#numEsc')
-    
-    for(c=0;num <= 20;c++){
-        var res = document.querySelector('#res')
-        var cnum = document.createElement('p',num)
-        res.innerHTML = cnum
-    
-    }
+function contar(){
+
+var ini = document.querySelector('#txti')
+
+
+
+
 }

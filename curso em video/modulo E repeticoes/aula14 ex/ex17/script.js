5069c59b298e96f951fb9143f14b616a9de0b6d7
eduzzobe <70654895+eduzzobe@users.noreply.github.com>
2020-12-16 13:16:13 -0300

    ex 17 pt 2

---
+++ curso em video/modulo E repeticoes/aula14 ex/ex17/script.js
@@ -0,0 +1,16 @@
+function tabuada(){
+
+    let ini = document.querySelector('#txti')
+    let res = document.querySelector('#res')
+
+    if(ini.value == 0){
+        alert('[Erro] Insira um número válido')
+    } else {
+        let tabu = document.querySelector('#tab')
+        let i = Number(ini.value)
+
+        
+
+    }
+
+}

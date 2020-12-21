166e9984aebe848df78eed7fbe0d1c7037fd1584
eduzzobe <70654895+eduzzobe@users.noreply.github.com>
2020-12-21 13:12:30 -0300

    modulo F start

---
+++ curso em video/modulo f avancado/aula15/ambiente.js
@@ -0,0 +1,10 @@
+// let num = [5,8,2,9,3]
+
+// console.log(`Nosso vetor é o ${num}`)
+
+let num = [5,8,2,9,3]
+num.sort()
+console.log(`O vetor dem ${num.length} posições`)
+console.log(num)
+
+//17:33

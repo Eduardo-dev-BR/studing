2eb054b261d68bb279662ae6b728b63a9eb51e53
eduzzobe <70654895+eduzzobe@users.noreply.github.com>
2020-12-23 14:16:45 -0300

    aula vetores

--- curso em video/modulo f avancado/aula15/ambiente.js
+++ curso em video/modulo f avancado/aula15/ambiente.js
@@ -3,8 +3,15 @@
 // console.log(`Nosso vetor é o ${num}`)
 
 let num = [5,8,2,9,3]
-num.sort()
-console.log(`O vetor dem ${num.length} posições`)
+num.push(1) //adiciona um numero no começo ou final
+num.sort() // exibe na ordem
 console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(4)
 
-//17:33
+if (pos == -1){
+    console.log('[ERRO] O valor não foi encontrado!')
+}else{
+console.log(`O valor está na posição $[pos]`)
+}

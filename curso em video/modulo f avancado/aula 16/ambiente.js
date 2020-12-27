2aefb81e2e6590a4861c37406227cd6532729201
Eduardo <webdu15@gmail.com>
2020-12-26 22:57:04 -0300

    Fim do curso JS curso em video

---
+++ curso em video/modulo f avancado/aula 16/ambiente.js
@@ -0,0 +1,39 @@
+// function parimp(n){
+//     if(n%2==0) {
+//         return 'par'
+//     } else {
+//         return 'impar'
+//     }
+// }
+
+// // let res = parimp(11)
+// // console.log(res)
+
+// // console.log(parimp(224))
+
+// function soma(n1=0,n2=0){
+//     return n1+n2
+// }
+// console.log(soma(7))
+
+// let v = function(x){
+//     return x*2
+// }
+
+// console.log(v(5))
+
+// 5! = 5*4*3*2*1
+
+// function fatorial(n){
+//     let fat = 1
+//     for (let c = n; c > 1; c--){
+//         fat *= c
+//     }
+//     return fat
+// }
+
+// console.log(fatorial(5))
+
+// function fatorial(n){
+    //recursividade
+// }

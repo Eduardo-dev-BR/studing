6e8d4df4e244f45bc634d2a93f272e25ff821325
eduzzobe <70654895+eduzzobe@users.noreply.github.com>
2021-02-01 19:34:29 -0300

    up danki

--- danki code/jQuery organizado/aula nav sem att/functions.js
+++ danki code/jQuery organizado/aula nav sem att/functions.js
@@ -1,12 +1,25 @@
 $(function(){
 
-    verificarCliqueMenu();
+    verificarcliqueMenu();
 
-    function verificarCliqueMenu(){
-        $('a').click(function (){
+    function verificarcliqueMenu(){
+        $('a').click(function(){
             var href = $(this).attr('href');
             $.ajax({
-                'url': href
+                'beforeSend': function(){
+                    console.log("Estamos chamando o beforeSend!");
+                },
+                'tumeout': 10000,
+                'url': href,
+                'error': function(jqXHR,textStatus,errorThrown){
+                    if(jqXHR.statusText =='Not Found'){
+                        console.log("Página nao encontrada.")
+                    }
+                },
+                'success':function(data){
+                    $('#container').html('');
+                    $(data).appendTo('#container').fadeIn();
+                }
             })
             return false;
         })

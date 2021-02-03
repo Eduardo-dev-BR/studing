10ebbeba0209fc65910e3b99e94ef7cc0007e07f
eduzzobe <70654895+eduzzobe@users.noreply.github.com>
2021-02-03 13:30:15 -0300

    danki up
    slider pronto

--- danki code/jQuery organizado/slide/functions.js
+++ danki code/jQuery organizado/slide/functions.js
@@ -2,7 +2,7 @@ $(function(){
 
     
     var indiceAtual = 0;
-    var indiceMaximo = $('.slider img').size();
+    var indiceMaximo = $('.slider img').length;
     var delay = 3000;
 
     /*initSlider();
@@ -18,11 +18,17 @@ $(function(){
     initSlider();
      function initSlider(){
          $('.slider img').eq(0).fadeIn();
-         setInterval(alternarSlider(),delay)
+         setInterval(function(){
+            alternarSlider()
+         },delay)
      }
 
      function alternarSlider(){
-
+        $('.slider img').eq(indiceAtual).fadeOut(2000);
+        indiceAtual+=1
+        if(indiceAtual == indiceMaximo)
+            indiceAtual = 0
+        $('.slider img').eq(indiceAtual).fadeIn(2000);
      }
 
 

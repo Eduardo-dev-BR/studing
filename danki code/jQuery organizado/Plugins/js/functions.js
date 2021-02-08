520192e6996178f0fa7c3eb2311707ff76a7eb57
eduzzobe <70654895+eduzzobe@users.noreply.github.com>
2021-02-08 13:36:49 -0300

    danki plugins pt1

--- danki code/jQuery organizado/Plugins/js/functions.js
+++ danki code/jQuery organizado/Plugins/js/functions.js
@@ -1,9 +1,11 @@
 $(function(){
-
+    // Plugin Social Media
     $('#social-share').jsSocials({
         shares: ["twitter", "facebook", "whatsapp"]
     })
 
+    //Plugin mask
+    $('input[name=telefone]').mask('(99)99999-9999');
 
 
 

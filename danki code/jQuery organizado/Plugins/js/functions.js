a9a98fb07fd416096a59763f51ae15749f1cdb36
eduzzobe <70654895+eduzzobe@users.noreply.github.com>
2021-02-11 13:38:09 -0300

    danki plugins finished

--- danki code/jQuery organizado/Plugins/js/functions.js
+++ danki code/jQuery organizado/Plugins/js/functions.js
@@ -1,13 +1,31 @@
 $(function(){
     // Plugin Social Media
     $('#social-share').jsSocials({
-        shares: ["twitter", "facebook", "whatsapp"]
+        shares: ["twitter", "whatsapp","facebook"]
     })
 
     //Plugin mask
     $('input[name=telefone]').mask('(99)99999-9999');
 
-
-
+    $('[data-fancybox]').fancybox({
+        protect: true,
+        buttons : [
+          'zoom',
+          'thumbs',
+          'close'
+        ]
+      });
+      
+      //Bloqueio
+      $('[data-fancybox="watermark"]').fancybox({
+        protect    : true,
+        slideClass : 'watermark',
+        toolbar    : false,
+        smallBtn   : true
+      });
+      
+      // Preload watermark image
+      // Please, use your own image
+      (new Image()).src = "https://fancyapps.com/GJbkSPU.png";
 
 })
